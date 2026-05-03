4bae0e8f472c0cb12afb5cd01a76be7fdd5d19bc
Kunal Madoliya <madoliyakunal2005@gmail.com>
2026-05-03 11:19:07 +0530

    fix: update server listen address to allow external connections

--- 1millioncheckbox/index.js
+++ 1millioncheckbox/index.js
@@ -84,8 +84,8 @@ async function main() {
       res.json({ checkboxes: state.checkboxes });
    });
 
-   server.listen(PORT, () => {
-      console.log(`Server running at http://localhost:${PORT}`);
+   server.listen(PORT, "0.0.0.0", () => {
+      console.log(`Server running on port ${PORT}`);
    });
 }
 
